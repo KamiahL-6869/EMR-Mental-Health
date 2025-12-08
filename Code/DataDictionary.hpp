a8049fdd5270bc2a7094064e2fa7d5069fac5d0f
Kamiah Long <klong16@leomail.tamuc.edu>
2025-12-07 20:36:06 -0600

    header

---
+++ Code/DataDictionary.hpp
@@ -0,0 +1,50 @@
+/** @file DataDictionary.hpp
+ * @brief Header file with definitions of concrte DataDictionary implementation
+ *   of the Dictionary container abstraction
+ *
+ * @author Kamiah Long
+ * @note   Project - Dallas Clinic, Fall 2025
+ * @note   IDE  : VSCode Server 3.9.3, Gnu Development Tools
+ * @note   C++ Language Standard Version: C++17
+ * @date   December 7, 2025
+ *
+ * Declares a Data Dictionary.
+ */
+#ifdef _DATA_DICTIONARY_HPP_
+#define _DATA_DICTIONARY_HPP_
+
+#include <string>
+#include <iostream>
+using namespace std; 
+
+/** @class Data Dictionary
+ * @brief Concreate implementation of a data dictionary for EMR datasets
+ * 
+ * @note Scans a sheet called "EMR_Data" and produces a new 
+ * sheet called "Data Dictionary" describing each field.
+ */
+template<class D>
+class DataDictionary : public Dictionary<D> {
+public:
+    /** @brief Constructor
+     * @param sourceSheetName Name of the source sheet with EMR dataset
+     * @param dictSheetName Name of the output sheet for the data dictionary
+     * @param sampleRows Number of rows to sample from the source sheet
+     */
+    DataDictionary(const string& sourceSheetName,
+                   const string& dictSheetName,
+                   size_t sampleRows); 
+    protected:
+    /** @brief Infer data type based on values
+     * @param values Vector of values to analyze
+     * @return Inferred data type as a string
+     */
+    string inferType(const vector<D>& values);  
+    /** @brief Test if a string is a date
+     * @param str String to test
+     * @return True if the string is a valid date, false otherwise
+     */
+    bool isValidDate(const string& str);
+}; 
+
+#endif // _DATA_DICTIONARY_HPP_
