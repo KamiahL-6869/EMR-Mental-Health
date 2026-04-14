8b5eb001eea20e136cd5257b4f5110886ffec60e
Kamiah Long <klong16@leomail.tamuc.edu>
2026-04-14 03:29:05 +0000

    login

--- Code/Mikes web page code/obj/Debug/net10.0/EMRweb.AssemblyInfo.cs
+++ Code/Mikes web page code/obj/Debug/net10.0/EMRweb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMRweb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af07152a464186bbdf30733fc41b4867a6a4486b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b36027e940a1b33e5e89c526720b8321495cf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMRweb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMRweb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
